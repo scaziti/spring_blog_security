88340bbbbb7dea57bffd7fc16d9c381436fa1d3d
Raphael Scaziti <rscaziti@gmail.com>
2021-03-02 14:43:22 -0300

    feat(usuario/controller): Adicionado controller para usuario

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
@@ -0,0 +1,40 @@
+package org.generation.blogPessoal.controller;
+
+import java.util.Optional;
+
+import org.generation.blogPessoal.model.UserLoginModel;
+import org.generation.blogPessoal.model.UsuarioModel;
+import org.generation.blogPessoal.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class UsuarioController 
+{
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@PostMapping("/logar")
+	public ResponseEntity<UserLoginModel> Autentication(@RequestBody Optional<UserLoginModel> user)
+	{
+		return this.usuarioService.Logar(user)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+	
+	@PostMapping("/cadastrar")
+	public ResponseEntity<UsuarioModel> Post(@RequestBody UsuarioModel usuario)
+	{
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(this.usuarioService.CadastrarUsuario(usuario));
+	}
+}
